54903393830818843b182b708fbc1418823ca4ea
Richar Ochoa <102420653+RicharOR@users.noreply.github.com>
2022-06-22 23:13:17 +0200

    [x]Corregido error en el título al cambiar de resolución

--- js/parallax.js
+++ js/parallax.js
@@ -1,27 +1,31 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    const windowWidth = $(window).width();
+    const windowWidth = $(window).width()
 
     if (windowWidth > 800) {
-      let scroll = $(window).scrollTop();
+      let scroll = $(window).scrollTop()
 
       $('header .textos').css({
         transform: `translate(0px, ${scroll / 2.5}%)`,
-      });
+      })
       $('.acerca-de article').css({
         transform: `translate(0px, -${scroll / 10}%)`,
-      });
+      })
     }
-  });
+  })
   $(window).resize(function () {
-    const windowWidth = $(window).width();
+    const windowWidth = $(window).width()
     if (windowWidth < 800) {
       $('header .textos').css({
         transform: `translate(0px, 0px)`,
-      });
+      })
       $('.acerca-de article').css({
         transform: `translate(0px, 0px)`,
-      });
+      })
     }
-  });
-});
+    $('header .textos').css({
+      opacity: 1,
+      marginTop: '52px',
+    })
+  })
+})
